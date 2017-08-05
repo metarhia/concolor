ea0c9fb5f43ddb4c45cc2eb1f88f79b786cbc8c0
tshemsedinov <timur.shemsedinov@gmail.com>
2017-08-05 15:55:54 +0300

    Version 0.1.2
    - Create semantic template tags
    - Fix processing not stylized expressions
    - Add tests

--- test.js
+++ test.js
@@ -2,15 +2,19 @@
 
 const concolor = require('./concolor.js');
 
-console.log(concolor`Hello ${'World'}(i,black/green) black on green`);
-console.log(concolor`Hello ${'World'}(blue) blue`);
-console.log(concolor`Hello ${'World'}(/red) on red`);
-console.log(concolor`Hello ${'World'}(white/yellow,b) bold white on yellow`);
-console.log(concolor`Hello ${'World'}(b,i) bold italic`);
-console.log(concolor`Hello ${'World'}(b,/blue) bold on blue`);
-console.log(concolor`Hello ${'World'}(b,u,yellow) bold underline yellow`);
-console.log(concolor`Hello ${'World'}(blue,u) blue underline`);
-console.log(concolor`Hello ${'World'}(b,black/green) bold black on green`);
+console.log('Examples:\n');
+
+console.log(concolor`  Hello ${'World'}(i,black/green) black on green`);
+console.log(concolor`  Hello ${'World'}(blue) blue`);
+console.log(concolor`  Hello ${'World'}(/red) on red`);
+console.log(concolor`  Hello ${'World'}(white/yellow,b) bold white on yellow`);
+console.log(concolor`  Hello ${'World'}(b,i) bold italic`);
+console.log(concolor`  Hello ${'World'}(b,/blue) bold on blue`);
+console.log(concolor`  Hello ${'World'}(b,u,yellow) bold underline yellow`);
+console.log(concolor`  Hello ${'World'}(blue,u) blue underline`);
+console.log(concolor`  Hello ${'World'}(b,black/green) bold black on green`);
+
+console.log('\nStyle examples:');
 
 console.log(concolor`
   ${'b for bold'}(b),
@@ -23,8 +27,23 @@ console.log(concolor`
   ${'s for strikethrough'}(s)`
 );
 
+console.log('\nComplex examples:');
+
 console.log(concolor`
   Client on ${'192.168.1.1'}(black/white)
   connected to ${'SERVER'}(n,b,red)
   at ${new Date().toUTCString()}(i,b,blue)
+  and variable with no style: ${'abc'}
 `);
+
+console.log('\nCreate semantic template tags:\n');
+
+const warn = concolor('b,yellow');
+const err = concolor('b,yellow/red');
+const inf = concolor('i,white');
+
+console.log('  ' + warn`test1 ${'text2'} text3`);
+console.log('  ' + err`test4 ${'text5'} text6`);
+console.log('  ' + inf`test7 ${'text8'} text9`);
+
+console.log('\nPassed\n');
